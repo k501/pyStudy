124af9566652c0d47da14b432aff9bd932202bde
k501 <komai19850822@gmail.com>
2017-08-18 01:04:57 +0900

    udemy

---
+++ udemy_python/pandas.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+import webbrowser
+
+website = 'http://en.wikipedia.org/wiki/NFL_win-loss_records'
+webbrowser.open(website)
+
+
+# クリップボードから読み込むことが可能です。
+nfl_frame = pd.read_clipboard()
+
+nfl_frame.head(4)
+nfl_frame.tail()
+nfl_frame.columns
+nfl_frame['Team']
+
+DataFrame(nfl_frame,  columns=['Team', 'Division'])
+
+# 新しいカラムを追加
+nfl_frame['Stadium'] = "Levis Stadium"
+nfl_frame['Stadium'] = np.arange(21)
+
+# Seriesを作成して、新しいカラムを作成
+stadiums = Series(["Levis Stadium", "AT&T Stadium"], [1,0])
+nfl_frame['Stadium'] = stadiums
+
+# カラムの削除
+del nfl_frame['Stadium']
+
+
+
+
+
+
+
+
+
+
